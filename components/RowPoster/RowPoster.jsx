fd2ee7cbf44a769fa6da09e25d89afc12be6bad9
fnxL <ajaykanki18@gmail.com>
2021-09-16 02:26:31 +0530

    Finished with Row

--- components/RowPoster/RowPoster.jsx
+++ components/RowPoster/RowPoster.jsx
@@ -1,9 +1,17 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { FaChevronDown, FaMinus, FaPlay, FaPlus } from 'react-icons/fa';
+import {
+  FaChevronDown,
+  FaMinus,
+  FaPlay,
+  FaPlus,
+  FaThumbsDown,
+  FaThumbsUp,
+} from 'react-icons/fa';
 import useGenreConversion from '../../hooks/useGenreConversion';
 import useStore from '../../store/store';
 import { getFallBackTitle } from '../../utils/utils';
+import Button from '../Button/Button';
 
 const BACKDROP_URL = process.env.BACKDROP_URL;
 const FALLBACK_URL = process.env.FALLBACK_URL;
@@ -29,92 +37,9 @@ function RowPoster(result) {
   let fallbackTitle = getFallBackTitle(item);
   const genresConverted = useGenreConversion(genre_ids);
 
-  const setModalContent = useStore((state) => state.setModalContent);
-
-  const handleModalOpening = (e) => {
-    e.stopPropagation();
-    setModalContent({
-      ...item,
-      fallbackTitle,
-      genresConverted,
-      type,
-      isFavourite,
-    });
-  };
-
-  const handleRedirect = () => {
-    router.push(`/${type}/${id}`);
-  };
-
   return (
-    <div
-      className={`Row__poster relative w-full h-44 ${isLarge && 'h-[461px]'}`}
-      onClick={handleRedirect}
-    >
-      {isLarge ? (
-        poster_path ? (
-          <Image
-            src={`${BACKDROP_URL}${poster_path}`}
-            alt={fallbackTitle}
-            layout='fill'
-            objectFit='cover'
-          />
-        ) : (
-          ''
-        )
-      ) : backdrop_path ? (
-        <Image
-          src={`${BACKDROP_URL}${backdrop_path}`}
-          alt={fallbackTitle}
-          layout='fill'
-          objectFit='cover'
-        />
-      ) : (
-        <>
-          <Image
-            src={FALLBACK_URL}
-            alt={fallbackTitle}
-            layout='fill'
-            objectFit='cover'
-          />
-          <div className='Row__poster__fallback'>
-            <span>{fallbackTitle}</span>
-          </div>
-        </>
-      )}
-      <div className='Row__poster-info'>
-        <div className='Row__poster-info--iconswrp'>
-          <button className='Row__poster-info--icon icon--play'>
-            <FaPlay />
-          </button>
-          {!isFavourite ? (
-            <button className='Row__poster-info--icon icon--favourite'>
-              <FaPlus />
-            </button>
-          ) : (
-            <button className='Row__poster-info--icon icon--favourite'>
-              <FaMinus />
-            </button>
-          )}
-          <button
-            onClick={handleModalOpening}
-            className='Row__poster-info--icon icon--toggleModal'
-          >
-            <FaChevronDown />
-          </button>
-        </div>
-        <div className='Row__poster-info--title'>
-          <h3>{fallbackTitle}</h3>
-        </div>
-        <div className='Row__poster-info--genres'>
-          {genresConverted &&
-            genresConverted.map((genre) => (
-              <span key={`Genre--id_${genre}`} className='genre-title'>
-                {genre}
-              </span>
-            ))}
-        </div>
-      </div>
+    <div className='movie'>
+      <img src={`${BACKDROP_URL}${backdrop_path}`} alt='' />
     </div>
   );
 }
