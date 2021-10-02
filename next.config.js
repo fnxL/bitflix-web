ae397ba20eb612201b2d3e2d553e9b030a4151a2
fnxL <ajaykanki18@gmail.com>
2021-10-02 19:09:39 +0530

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /* eslint-disable object-shorthand */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      'assets.stickpng.com',
-      'images.pexels.com',
-      'image.tmdb.org',
-      'ih1.redbubble.net',
-      'i.stack.imgur.com',
-    ],
-  },
   env: {
     TMDB: 'e47e6864261ea10181fc486b7cf6aba5',
     FANART: '3a565b507ab4255eb92cb933687f1064',
@@ -21,13 +12,13 @@ module.exports = {
     POSTER_URL: 'https://image.tmdb.org/t/p/w342',
     FALLBACK_URL: 'https://i.stack.imgur.com/y9DpT.jpg',
     PROXY_URL: 'http://127.0.0.1:3000/api', // cloudflare end url
-    SERVER_URL: 'http://localhost:5000/api',
+    SERVER_URL: 'https://bit-flix.herokuapp.com/api',
   },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*', // server url, proxy requests to backend
+        destination: 'https://bit-flix.herokuapp.com/api/:path*', // server url, proxy requests to backend
       },
     ];
   },
